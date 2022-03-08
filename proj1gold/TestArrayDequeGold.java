669751bbf364b9e4a63c29c9f572846fcf62a2bc
hll <460838554@qq.com>
2022-03-08 14:27:14 +0800

    try DequeGold

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,36 @@
+import  org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestArrayDequeGold {
+
+    StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+    ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
+
+    @Test
+    public void testArray() {
+        while (true) {
+            int i = StdRandom.uniform(10);
+            double flag = StdRandom.uniform();
+
+            if (flag < 0.5 && !student.isEmpty()) {
+                if (flag < 0.25) {
+                    Integer x = student.removeFirst();
+                    Integer y = solution.removeFirst();
+                    assertEquals("removeFirst()\n", x, y);
+                } else {
+                    Integer x = student.removeLast();
+                    Integer y = solution.removeLast();
+                    assertEquals("removeLast\n", x, y);
+                }
+            } else if (flag >= 0.5) {
+                if (flag < 0.75) {
+                    student.addFirst(i);
+                    solution.addFirst(i);
+                } else {
+                    student.addLast(i);
+                    solution.addLast(i);
+                }
+            }
+        }
+    }
+}
